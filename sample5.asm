a094bf90046b141c6df0e9db4167ee585bf7d15f
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-05-12 23:58:03 +0700

    Update sample5.asm

--- sample5.asm
+++ sample5.asm
@@ -19,15 +19,25 @@ main proc ; vidu 5
   mov ah,10
   lea dx,xau
   int 21h
-  
+
+  mov ah,9
   lea dx,xd
   int 21h
 
-  mov ah,9
   lea dx,xuat
   int 21h
-  call inthuong ;dung de goi ham con
+  call inthuong ;dung de goi ham con inthuong
 
+  mov ah,9
+  lea dx,xd
+  int 21h
+
+  lea dx,xuat2
+  int 21h
+  call inhoa ; goi ham con inhoa
+            
+  mov ah,0 ;ham return
+  int 21h
   
 
 main endp
@@ -35,11 +45,11 @@ inthuong proc
     lea si,xau+2 ; thanh ghi si tro den dia chi thu 2 trong xau
     looptu:
       mov dl,[si] ; [si] la gia tri noi dung ma thanh ghi si dang tro den, goi dl de in ra bang lenh ah,2
-      cmp dl, 'a' ; so sanh dl voi ki tu a
+      cmp dl, 'A' ; so sanh dl voi ki tu A
       jl in1 ; jn la jump less, neu dl nho hon a thi chay lenh in1
-      cmp dl, 'z' ; so sanh dl voi z
+      cmp dl, 'Z' ; so sanh dl voi Z
       jg in1 ; jg la jump greater, neu dl lon hon z thi chay lenh in1
-      sub dl,32 ; neu cac ham so sanh tren sai thi chay lenh nay
+      add dl,32 ; neu cac ham so sanh tren sai thi chay lenh nay
     in1:
       mov ah,2 ; ham in 1 ky tu duoc luu trong thanh ghi dl
       int 21h
@@ -48,4 +58,22 @@ inthuong proc
       jne looptu ; jump not equal tuong tu nhu phep so sanh != trong c++
   ret ; quan trong, ham con luon co ret trong ham
   inthuong endp
+
+inhoa proc
+    lea si,xau+2 ; thanh ghi si tro den dia chi thu 2 trong xau
+    looptu1:
+      mov dl,[si] ; [si] la gia tri noi dung ma thanh ghi si dang tro den, goi dl de in ra bang lenh ah,2
+      cmp dl, 'a' ; so sanh dl voi ki tu a
+      jl in2 ; jn la jump less, neu dl nho hon a thi chay lenh in1
+      cmp dl, 'z' ; so sanh dl voi z
+      jg in2 ; jg la jump greater, neu dl lon hon z thi chay lenh in1
+      sub dl,32 ; neu cac ham so sanh tren sai thi chay lenh nay
+    in2:
+      mov ah,2 ; ham in 1 ky tu duoc luu trong thanh ghi dl
+      int 21h
+      inc si ; tuong tu si++, tang con tro len 1 don vi
+      cmp [si], '$' ; so sanh [si] co phai ky tu ket thuc xau hay khong
+      jne looptu1 ; jump not equal tuong tu nhu phep so sanh != trong c++
+  ret ; quan trong, ham con luon co ret trong ham
+  inhoa endp
 end
