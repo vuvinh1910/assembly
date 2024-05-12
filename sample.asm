47e0682da86e17c820e973f63d8289436df16ac3
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-05-12 22:06:15 +0700

    Update sample.asm

--- sample.asm
+++ sample.asm
@@ -1,8 +1,8 @@
 .model small
 .Stack 100h
 .Data
-     ; ;  Cú pháp trong .data:
-     ; ; <tên biến> d<Kiểu DL> <giá trị khởi tạo>
+     ;  Cú pháp trong .data:
+     ; <tên biến> d<Kiểu DL> <giá trị khởi tạo>
      xuongdong DB 13,10,'$' ; trong ascii 13 la ki tu quay lai dau dong,10 la xuong dong,'$' danh dau ket thuc chuoi.
      ; DB la double word thuong de luu ky tu
      xinchao DB 'hello$'
@@ -13,10 +13,13 @@ Main proc ;vidu
   ; quan trọng, học thuộc, tác dụng là trỏ đến .Data ở trên
   
   mov ah,9 ;gọi hàm in ra xâu kí tự trong hàm hệ thống 
+  lea dx,xinchao ;khi sử dụng dịch vụ hệ thống để in chuỗi ra màn hình thông qua int 21h, chuỗi cần phải được trỏ bởi thanh ghi DX. Điều này là quy ước do cách DOS xử lý các hàm gọi hệ thống.
+  int 21h ;Khi bạn sử dụng lệnh int 21h, CPU sẽ chuyển quyền kiểm soát tới hệ thống , 
+     ;và nó sẽ thực hiện một hoạt động cụ thể dựa trên giá trị được đặt trong thanh ghi AH.
+     ;Giá trị này xác định chức năng cụ thể mà bạn muốn thực hiện.
   
   ; 2 dong sau tuong tu return trong c++;
   mov ah,4ch
   int 21h
-  ; Khi bạn sử dụng lệnh int 21h, CPU sẽ chuyển quyền kiểm soát tới hệ thống,
-  ; và nó sẽ thực hiện một hoạt động cụ thể dựa trên giá trị được đặt trong thanh ghi AH. trước đó là ah,9.
-  ; Giá trị này xác định chức năng cụ thể mà bạn muốn thực hiện.
+Main Endp
+END
