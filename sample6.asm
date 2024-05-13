f47148114c38cfee2f7b0a8e2ce956bf18d436c2
vuvinh1910 <108977375+vuvinh1910@users.noreply.github.com>
2024-05-13 17:29:55 +0700

    Update sample6.asm

--- sample6.asm
+++ sample6.asm
@@ -9,7 +9,8 @@
 main proc ; vidu 6
   mov ax,@data
   mov ds,ax
-  mov cx,0
+  mov cx,0 ; sử dụng loop sẽ căn cứ vào cx, cứ mỗi lần loop thì cx--, nếu cx=0 thì dừng lại vòng lặp
+           ; khai báo cx=0 để xác định cx, nếu không dòng 31 sẽ lỗi, vidu lỗi: cx=256 thay vì đúng là 0 sau khi gán xau+1 cho cl
   
   mov ah,9
   lea dx,nhap
@@ -27,19 +28,19 @@ main proc ; vidu 6
   lea dx,xd
   int 21h
   
-  mov cl,[xau+1]
-  
-  lea si,[xau+2]
+  mov cl,[xau+1]  ; cl để lưu byte kí tự,cx thường lưu số nguyên, gán cl cho số phần tử của xau, thì cx sẽ thay đổi thành số kí tự của xau
+                  ; cl = '3' cx = 3
+  lea si,[xau+2]  ; con trỏ si trỏ tới phần tử đầu tiên của xâu
   duyetxau:
     push [si]
     inc si
     loop duyetxau
   
-  mov cl,[xau+1]
+  mov cl,[xau+1] ; gán lại cl và cx vì cx ở loop trước đã trừ hết = 0
   inxau1:
-    pop dx;
+    pop dx ; pop phần tử của stack và đưa vào dx  
     mov ah,2
-    int 21h;
+    int 21h ; hàm ah,2 in ra dl mà dl là thanh ghi con trong dx
     loop inxau1
    
   mov ah,0
